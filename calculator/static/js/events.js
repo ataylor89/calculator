16ecd518a7e047fb95df7716c5ed950a8f266094
Andrew Taylor <rolyata47@gmail.com>
2025-07-13 17:10:27 -0500

    Removing log statements

--- calculator/static/js/events.js
+++ calculator/static/js/events.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
 
 function eval(e) {
     e.preventDefault();
-    console.log("Calling the eval function...");
 
     $.post('/evaluate', $('#inputform').serialize(), function(data) {
         $('#result').html(data['result']);
