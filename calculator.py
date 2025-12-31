fa985502f979256a74211b8fd5c1df1d2859e2d2
Andrew Taylor <rolyata47@gmail.com>
2025-12-31 17:00:11 -0600

    Adding calculator.py

---
+++ calculator.py
@@ -0,0 +1,17 @@
+from strategies import strategy1
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(prog='calculator.py', description='Evaluate an arithmetic expression')
+    parser.add_argument('expression', type=str)
+    parser.add_argument('-s', '--strategy', type=int, default=1)
+    args = parser.parse_args()
+    if args.strategy < 1 or args.strategy > 3:
+        print('Strategy not supported.')
+        return
+    if args.strategy == 1:
+        result = strategy1.eval(args.expression)
+        print(result)
+
+if __name__ == '__main__':
+    main()
