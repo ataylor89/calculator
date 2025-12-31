49ee60f25ff04bf1f9656f8e20691fcc6f059cb3
Andrew Taylor <rolyata47@gmail.com>
2025-12-31 17:03:21 -0600

    Making this a libary

--- strategies/strategy1.py
+++ strategies/strategy1.py
@@ -1,5 +1,3 @@
-import sys
-
 digits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
 operators = {'+', '-', '*', '/', '^'}
 parentheses = {'(', ')'}
@@ -119,14 +117,3 @@ def simplify(tokens):
         del tokens[index:index+2]
 
     return simplify(tokens)
-
-def main():
-    if len(sys.argv) < 2:
-        print('Usage: python %s <expression_enclosed_in_quotes>' %sys.argv[0])
-        return
-    expression = sys.argv[1]
-    result = eval(expression)
-    print(result)
-
-if __name__ == '__main__':
-    main()
