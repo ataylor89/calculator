d4bd134b24a1163f6ece5f91eba7cdb070741adc
Andrew Taylor <rolyata47@gmail.com>
2025-12-31 20:56:48 -0600

    Changing super to super()

--- strategies/exceptions.py
+++ strategies/exceptions.py
@@ -1,7 +1,7 @@
 class InvalidExpression(Exception):
     def __init__(self, expression, message='The expression is invalid'):
-        super.__init__(message)
+        super().__init__(message)
 
 class InvalidStrategy(Exception):
     def __init__(self, expression, message='Strategy not supported'):
-        super.__init__(message)
+        super().__init__(message)
