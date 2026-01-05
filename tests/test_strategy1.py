650d847c1710a6b7331e7c78d39490e50f41acff
Andrew Taylor <rolyata47@gmail.com>
2026-01-05 13:42:06 -0600

    Adding a new unit test for evaluating postfix expressions

--- tests/test_strategy1.py
+++ tests/test_strategy1.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from strategies.strategy1 import Strategy1
 from strategies.exceptions import InvalidExpression
-from tests.test_data import valid_inputs, invalid_inputs, infix_to_postfix
+from tests.test_data import valid_inputs, invalid_inputs, infix_to_postfix, eval_postfix
 
 class TestStrategy1(TestCase):
     
@@ -21,5 +21,11 @@ class TestStrategy1(TestCase):
     def test_infix_to_postfix(self):
         strategy = Strategy1()
         test_data = infix_to_postfix.parse()
-        for (infix, postfix) in test_data:
-            assert strategy.convert_to_postfix(infix) == postfix
+        for (test_input, desired_output) in test_data:
+            assert strategy.convert_to_postfix(test_input) == desired_output
+
+    def test_eval_postfix(self):
+        strategy = Strategy1()
+        test_data = eval_postfix.parse()
+        for (test_input, desired_output) in test_data:
+            assert strategy.eval_postfix(test_input) == desired_output
