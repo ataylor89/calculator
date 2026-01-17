a4287c8aa856bf4f5a217551ec08580c4345967b
Andrew Taylor <rolyata47@gmail.com>
2026-01-17 14:20:31 -0600

    Adding a while loop to perform calculations

--- calculator.py
+++ calculator.py
@@ -15,15 +15,27 @@ def eval(expression, strategy=1):
         strategy = Strategy3()
     return strategy.eval(expression)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog='calculator.py', description='Evaluate an arithmetic expression')
-    parser.add_argument('expression', type=str)
-    parser.add_argument('-s', '--strategy', type=int, default=1)
-    args = parser.parse_args()
+def eval_and_print(expression, strategy=1):
     try:
-        result = eval(args.expression, args.strategy)
+        result = eval(expression, strategy)
         print(result)
     except (InvalidExpression, InvalidStrategy) as e:
         print(e)
     except Exception as e:
         print('There was an error while evaluating the expression')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='calculator.py', description='Evaluate an arithmetic expression')
+    parser.add_argument('expression', type=str, nargs='?')
+    parser.add_argument('-s', '--strategy', type=int, default=1)
+    args = parser.parse_args()
+    expression = args.expression
+    strategy = args.strategy
+    if expression:
+        eval_and_print(expression, strategy)
+    else:
+        while True:
+            user_input = input().strip()
+            if not user_input or user_input == 'exit':
+                break
+            eval_and_print(user_input, strategy)
